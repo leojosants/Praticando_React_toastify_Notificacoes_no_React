a62f084eae0576ce848b10566469ff94d2118cc1
Leonardo Santos <leonardojosantos@gmail.com>
2023-07-11 11:04:58 -0300

    Finalizado

--- teste/src/pages/Notifications/index.js
+++ teste/src/pages/Notifications/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { toast } from 'react-toastify';
 import { Container, Buttons } from './styles';
 
-function Notifications () {
+function Notifications() {
 
     function handleDefault() {
         toast('Mensagem default');
@@ -10,61 +10,93 @@ function Notifications () {
 
 
     function handleError() {
-        toast.error('Mensagem error');
+        toast.error('Requisição mal sucessida!', {
+            position: "bottom-left",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+        });
     }
 
 
     function handleSuccess() {
-        toast.success('Mensagem success');
+        toast.success('Sua requisição foi bem sucessida!', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+        });
     }
 
 
     function handleInfo() {
-        toast.info('Mensagem info');
+        toast.info('Carregando...', {
+            position: 'bottom-right',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+            isLoading: true,
+        });
     }
 
 
     function handleWarn() {
-        toast.warn('Mensagem warn');
+        toast.warn('Cuidado!', {
+            position: "top-left",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            theme: 'dark',
+            progress: undefined,
+        });
     }
 
 
     function handleCustom() {
         toast('Mensagem customizada', {
-        position: toast.POSITION.TOP_LEFT,
-        className: 'sua-classe',
-    });
-}
+            position: toast.POSITION.BOTTOM_CENTER,
+            className: 'sua-classe',
+        });
+    }
 
-return (
+    return (
         <Container>
             <Buttons>
-            <button type="button" onClick={handleDefault}>
-                Default
-            </button>
+                <button type="button" onClick={handleDefault}>
+                    Default
+                </button>
 
-            <button type="button" onClick={handleError}>
-                Error
-            </button>
+                <button type="button" onClick={handleError}>
+                    Error
+                </button>
 
-            <button type="button" onClick={handleSuccess}>
-                Success
-            </button>
+                <button type="button" onClick={handleSuccess}>
+                    Success
+                </button>
 
-            <button type="button" onClick={handleWarn}>
-                Warn
-            </button>
+                <button type="button" onClick={handleWarn}>
+                    Warn
+                </button>
 
-            <button type="button" onClick={handleInfo}>
-                Info
-            </button>
+                <button type="button" onClick={handleInfo}>
+                    Info
+                </button>
 
-            <button type="button" onClick={handleCustom}>
-                Custom
-            </button>
-            
-        </Buttons>
-    </Container>
+                <button type="button" onClick={handleCustom}>
+                    Custom
+                </button>
+            </Buttons>
+        </Container>
     );
 }
 export default Notifications;
